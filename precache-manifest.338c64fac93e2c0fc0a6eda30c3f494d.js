1fbcc221e8a50cbe0a18cd6541f2ad53bb855b40
PhatOrbs <eddcollado@gmail.com>
2020-05-10 21:41:49 -0600

    Updates

---
+++ precache-manifest.338c64fac93e2c0fc0a6eda30c3f494d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842a4ceb6d7b6c0d96ea7f7463a228d6",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "1908b93039d431c1ac2f",
+    "url": "/React-Portfolio/static/css/2.59405167.chunk.css"
+  },
+  {
+    "revision": "3903ed4e8c4ad727d76c",
+    "url": "/React-Portfolio/static/css/main.05ab5169.chunk.css"
+  },
+  {
+    "revision": "1908b93039d431c1ac2f",
+    "url": "/React-Portfolio/static/js/2.a8aaf24c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.a8aaf24c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3903ed4e8c4ad727d76c",
+    "url": "/React-Portfolio/static/js/main.dd8a5908.chunk.js"
+  },
+  {
+    "revision": "945c8e00074ad2c67f4f",
+    "url": "/React-Portfolio/static/js/runtime-main.b7a87ec7.js"
+  },
+  {
+    "revision": "005881dcdde2fcd02ef6733146ae917b",
+    "url": "/React-Portfolio/static/media/Me.005881dc.jpg"
+  },
+  {
+    "revision": "6dfed9aee1f03d41e26dcd5e313aaca1",
+    "url": "/React-Portfolio/static/media/cocktail_mixtape1.6dfed9ae.gif"
+  },
+  {
+    "revision": "39fde42585ba6ba8c7636361119f8d79",
+    "url": "/React-Portfolio/static/media/github-icon_cyan.39fde425.png"
+  },
+  {
+    "revision": "904d64fa70984f6d19422419fec5c53a",
+    "url": "/React-Portfolio/static/media/index.904d64fa.less"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c0874b3af0d50fd1bf8fd8188d4e0123",
+    "url": "/React-Portfolio/static/media/quarantinee_app.c0874b3a.gif"
+  },
+  {
+    "revision": "abf95142968b8b066920d0ebd1ae7b68",
+    "url": "/React-Portfolio/static/media/weatherDashboard.abf95142.gif"
+  },
+  {
+    "revision": "6437b8737d207b6e1489853149d73b15",
+    "url": "/React-Portfolio/static/media/whiteresume.6437b873.jpg"
+  },
+  {
+    "revision": "ac82e4dbc46a592acc84c9ea4c9a235a",
+    "url": "/React-Portfolio/static/media/workDayScheduler.ac82e4db.gif"
+  }
+]);
